394d7a47e6d235525b6f2036e0b5a687f5809304
HoangHa <tuannguyenanh1412@gmail.com>
2021-07-14 10:15:53 +0700

    update 14/07/21

---
+++ weather/person.js
@@ -0,0 +1,6 @@
+// document.getElementById("btnPersonal").onclick = function() { showBodyBtnPerson() };
+
+
+function showBodyBtnPerson() {
+    document.getElementById("searchPerson").classList.toggle("show");
+}
