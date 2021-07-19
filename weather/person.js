5d7834cc0cc1cdab1410b5bffe7ace2ecfcb1299
HoangHa <tuannguyenanh1412@gmail.com>
2021-07-19 14:08:31 +0700

    update 17/09/21

--- weather/person.js
+++ weather/person.js
@@ -1,6 +1,11 @@
 // document.getElementById("btnPersonal").onclick = function() { showBodyBtnPerson() };
-
-
+// document.getElementById("btnAccount").onclick = function() { showBodyBtnAccount() };
 function showBodyBtnPerson() {
     document.getElementById("searchPerson").classList.toggle("show");
+}
+
+function showBodyBtnAccount() {
+    
+    document.getElementById("btnAccount").classList.toggle("showUser");
+    
 }
--- weather/scroll.js
+++ weather/scroll.js
@@ -2,8 +2,8 @@ const buttonRight = document.getElementById('slideRight');
     const buttonLeft = document.getElementById('slideLeft');
 
     buttonRight.onclick = function () {
-      document.getElementById('scroll').scrollLeft += 950;
+      document.getElementById('scroll').scrollLeft += 190;
     };
     buttonLeft.onclick = function () {
-      document.getElementById('scroll').scrollLeft -= 950;
+      document.getElementById('scroll').scrollLeft -= 190;
     };  
